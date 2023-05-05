4f51c43c5421eddbb2ad9ab7765ea693c45e5d69
Anthony Padfield <antrp16@gmail.com>
2023-05-04 18:46:27 -0700

    set objects of login into local storage

--- login.js
+++ login.js
@@ -1,3 +1,21 @@
-let username = document.getElementById('username')
-let password = document.getElementById('password')
-let Submit = document.getElementById('Submit')
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const submitButton = document.querySelector('#Submit');
+
+function saveUserdata(){
+    let user = document.getElementById(data).value;
+    let data = {
+      name:username.value.trim(),
+      password:password.value.trim()}
+    };
+    document.getElementById('Submit').addEventListener("click", function() {
+    localStorage.setItem('username',
+     JSON.stringify({username:username.value})
+     )
+  });
+  
+     document.getElementById('Submit').addEventListener("click", function() {
+      localStorage.setItem('password',
+     JSON.stringify({password:password.value}),
+     )
+  });
